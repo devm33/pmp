63df9f30606cf31bb28372d97f54319767dafcd6
Devraj Mehta <devm33@gmail.com>
2015-04-19 15:06:02 -0400

    broke out watch task into separate module

---
+++ gulp/watch.js
@@ -0,0 +1,25 @@
+var _ = require('lodash');
+var browserSync = require('browser-sync');
+var chalk = require('chalk');
+var gutil = require('gulp-util');
+var merge = require('merge-stream');
+var watch = require('gulp-watch');
+
+var config = require('./config');
+
+module.exports = function(pipes) {
+    return function() {
+        _.each(config.watch, function(watchcfg) {
+            watch(watchcfg.glob, function() {
+                gutil.log(
+                    'Watch triggered for', chalk.magenta(watchcfg.glob),
+                    'running', chalk.cyan(watchcfg.tasks.toString())
+                );
+                merge.apply(null, _.map(watchcfg.tasks, function(task) {
+                    return pipes[task]();
+                }))
+                .pipe(browserSync.reload({stream: true}));
+            });
+        });
+    };
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
-var _ = require('lodash');
 var browserSync = require('browser-sync');
-var chalk = require('chalk');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var merge = require('merge-stream');
-var rimraf = require('del');
+var del = require('del');
 var series = require('stream-series');
-var watch = require('gulp-watch');
 
 var config = require('./gulp/config');
 var pipes = require('./gulp/pipes')(gulp);
@@ -15,23 +11,10 @@ gulp.task('serve', ['build', 'watch'], function() {
     browserSync({server: {baseDir: './'}});
 });
 
-gulp.task('watch', ['build'], function() {
-    _.each(config.watch, function(watchcfg) {
-        watch(watchcfg.glob, function() {
-            gutil.log(
-                'Watch triggered for', chalk.magenta(watchcfg.glob),
-                'running', chalk.cyan(watchcfg.tasks.toString())
-            );
-            merge.apply(null, _.map(watchcfg.tasks, function(task) {
-                return pipes[task]();
-            }))
-            .pipe(browserSync.reload({stream: true}));
-        });
-    });
-});
+gulp.task('watch', ['build'], require('./gulp/watch')(pipes));
 
 gulp.task('clean', function(cb) { 
-    rimraf(config.dest, cb);
+    del(config.dest, cb);
 });
 
 gulp.task('build', ['clean'], function() {
