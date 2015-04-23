12ca0017fe670fb08d0a28fba4b78f4628d16d1a
Devraj Mehta <devm33@gmail.com>
2015-04-22 23:06:33 -0400

    handsontable still isnt working!

--- src/rates/rates.js
+++ src/rates/rates.js
@@ -17,4 +17,14 @@ angular.module('pmp/rates', [
     Rates.then(function(rates) {
         ctrl.rates = rates;
     });
+
+    var currentMonth = moment().month();
+    ctrl.months = _.times(5, function(index) {
+        var month = moment().month(currentMonth + index);
+        return {
+            data: month.format('MM_YYYY'),
+            title: month.format('MMM YYYY')
+        };
+    });
+    // ctrl.columns.unshift({ data: 'name', title: 'Name' });
 });
